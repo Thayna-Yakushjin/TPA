89bcdedfbefc43ef5a41e47dd4b5f34654694447
Thayna-Yakushjin <lhuluy77@gmail.com>
2025-10-07 11:54:04 -0300

    Add files via upload

---
+++ lista07/OrdemNumeros2.java
@@ -0,0 +1,22 @@
+package lk2;
+
+import java.util.Scanner;
+
+public class OrdemNumeros2 {
+
+	public static void main(String[] args) {
+		
+		Scanner in= new Scanner (System.in);
+        int i = 2;
+        do {
+            System.out.print(i + " - ");
+            i += 2;
+        } while (i <= 500);
+        
+        
+    }
+	
+
+	}
+
+
